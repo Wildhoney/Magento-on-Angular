262466161c77e1a7ed10eaa3b2aeadffb1356f4d
Adam Timberlake <adam.timberlake@gmail.com>
2013-09-22 21:53:48 +0100

    Grunt build

---
+++ dist/mao-0.0.3.js
@@ -0,0 +1,2 @@
+/*! Mao by Adam Timberlake created on 2013-09-22 */
+!function(a){"use strict";a.mao=angular.module("mao",[]),a.mao.config(["$routeProvider","$provide","$locationProvider",function(a,b,c){a.when("/",{templateUrl:"views/home.html"}).when("/category/:category",{templateUrl:"views/products.html"}).when("/category/:category/:subCategory",{templateUrl:"views/products.html"}).when("/product/:ident",{templateUrl:"views/product.html"}).otherwise({redirectTo:"/error/not-found"}),c.html5Mode(!1).hashPrefix("!")}])}(window);
