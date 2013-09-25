a73ca8c94fc882b6c5c6f70765e40e74e084b926
Adam Timberlake <adam.timberlake@gmail.com>
2013-09-26 00:00:57 +0100

    Added BasketController skeleton

---
+++ store/js/Basket/Controller.js
@@ -0,0 +1,15 @@
+(function($m) {
+
+    /**
+     * @controller BasketController
+     * @author Adam Timberlake
+     */
+    $m.controller('BasketController',
+
+        ['$rootScope', '$scope',
+
+        function BasketController($rootScope, $scope) {
+
+    }]);
+
+})(window.mao);
