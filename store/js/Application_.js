94695967f4d80771a45b03d56faff243615d0c99
Adam Timberlake <adam.timberlake@gmail.com>
2014-01-24 00:31:47 +0000

    Added ApplicationCtrl

--- store/js/Application_.js
+++ store/js/Application_.js
@@ -1,5 +1,9 @@
 (function($window, $angular) {
 
+    /**
+     * @property window.maoApp
+     * @type {Object}
+     */
     $window.maoApp = $angular.module('maoApp', []);
 
 })(window, window.angular);
---
+++ store/js/controllers/ApplicationController.js
@@ -0,0 +1,12 @@
+(function($mao) {
+
+    /**
+     * @controller ApplicationController
+     * @author Adam Timberlake
+     * @module Mao
+     */
+    $mao.controller('ApplicationController', ['$scope', function applicationController() {
+
+    }]);
+
+})(window.maoApp);
