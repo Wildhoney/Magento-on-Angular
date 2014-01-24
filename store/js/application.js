1688b71ab5d8cfbbf392b64acc95cbe5ea1da6fe
Adam Timberlake <adam.timberlake@gmail.com>
2014-01-24 00:18:26 +0000

    Added initial Angular bootstrap

--- store/js/application.js
+++ store/js/application.js
@@ -1,26 +1,5 @@
-(function($w) {
+(function($window, $angular) {
 
-    "use strict";
+    $window.maoApp = $angular.module('maoApp', []);
 
-    $w.mao = angular.module('mao', []);
-
-    /**
-     * @method config
-     * Response for configuring the routes.
-     */
-    $w.mao.config(['$routeProvider', '$provide', '$locationProvider',
-
-        function config($routeProvider, $provide, $locationProvider) {
-
-            $routeProvider
-                .when('/', { templateUrl: 'views/home.html' })
-                .when('/category/:category', { templateUrl: 'views/products.html' })
-                .when('/category/:category/:subCategory', { templateUrl: 'views/products.html' })
-                .when('/product/:ident', { templateUrl: 'views/product.html' })
-                .otherwise({ redirectTo: '/error/not-found' });
-
-            $locationProvider.html5Mode(false).hashPrefix('!');
-
-    }]);
-
-})(window);
+})(window, window.angular);
