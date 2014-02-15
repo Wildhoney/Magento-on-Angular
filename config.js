e69a2da48e0df1c30d7e49d4d7bea23dcc0446a2
Raja Kapur <raja.kapur@gmail.com>
2014-02-15 00:57:50 +0000

    central config file

---
+++ config.js
@@ -0,0 +1,15 @@
+{
+  'socket': {
+    'ip': '127.0.0.1', 
+    'port': 8888, 
+    'host': 'localhost'
+  },
+  'magento': {
+    'path': '/var/www/magento',
+    'host': 'http://localhost/magento',
+    'store': 0
+  },
+  'api': {
+    'host': 'http://localhost/api'
+  }
+}
