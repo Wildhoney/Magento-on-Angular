bfbf2fa9f8360ba5e16c5b2490a89233939a577f
Raja Kapur <raja.kapur@gmail.com>
2014-02-15 17:54:39 +0000

    tabs to spaces

--- api/app/controllers/BaseAPIController.php
+++ api/app/controllers/BaseAPIController.php
@@ -4,14 +4,14 @@ use Moa\API\Provider\ProviderInterface;
 
 class BaseAPIController extends BaseController {
 
-	protected $api;
+    protected $api;
 
     /**
      * @constructor
      */
     public function __construct(ProviderInterface $api) {
-		$this->api = $api;
-		$this->api->startSession();
+        $this->api = $api;
+        $this->api->startSession();
     }
 
 }
