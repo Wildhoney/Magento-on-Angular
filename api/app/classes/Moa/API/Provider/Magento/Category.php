feec0198e2877fb0fe06d2ed879c49c75475bdff
Rouz <coolanddeadly82@gmail.com>
2015-01-06 11:55:12 +0100

    fixed createIdent() method name, _createIdent() could not be found

--- api/app/classes/Moa/API/Provider/Magento/Category.php
+++ api/app/classes/Moa/API/Provider/Magento/Category.php
@@ -65,7 +65,7 @@ trait Category {
                 // Prepare the model for appending to the collection.
                 $model = array(
                     'id'            => (int) $subCategory->getId(),
-                    'ident'         => $this->_createIdent($subCategory->getName()),
+                    'ident'         => $this->createIdent($subCategory->getName()),
                     'name'          => $subCategory->getName(),
                     'productCount'  => $productCount($subCategory->getId()),
                 );
