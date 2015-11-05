93bdc663b1b1e2e5f90339d2d4401efb27872c6b
Vallabh Kansagara <vrkansagara@gmail.com>
2015-11-05 13:04:42 +0530

    Magento Service Provider added into application.

--- api/config/app.php
+++ api/config/app.php
@@ -145,7 +145,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        'Moa\Laravel\APIServiceProvider',
+        App\Providers\MagentoServiceProvider::class
 
     ],
 
