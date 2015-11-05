88002de32e91bdd8d8db1c28a712dd9f58d129c7
Vallabh Kansagara <vrkansagara@gmail.com>
2015-11-05 13:06:58 +0530

    BaseController removed from conflicting with base controller.

--- api/app/Http/Controllers/BaseAPIController.php
+++ api/app/Http/Controllers/BaseAPIController.php
@@ -1,18 +1,28 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Moa\API\Provider\ProviderInterface;
 
-class BaseAPIController extends BaseController {
+class BaseAPIController extends Controller
+{
 
     protected $api;
 
     /**
      * @constructor
      */
-    public function __construct(ProviderInterface $api) {
+    public function __construct(ProviderInterface $api)
+    {
         $this->api = $api;
         $this->api->startSession();
+    }
+
+    protected function setupLayout()
+    {
+        if (!is_null($this->layout)) {
+            $this->layout = View::make($this->layout);
+        }
     }
 
 }
